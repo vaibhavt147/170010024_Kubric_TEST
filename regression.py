b92b72126b044cd445b7002804b7ef2711dc57ca
Vaibhav Takale <52785362+vaibhavt147@users.noreply.github.com>
2020-11-19 23:27:36 +0530

    Update regression.py

--- regression.py
+++ regression.py
@@ -1,29 +1,34 @@
-import requests
-import pandas
-import scipy
-import numpy
+import pandas as pd
+import numpy as np
 import sys
+from sklearn.linear_model import LinearRegression
+
+TRAIN_DATA = "https://storage.googleapis.com/kubric-hiring/linreg_train.csv"
+TEST_DATA = "https://storage.googleapis.com/kubric-hiring/linreg_test.csv"
 
 
-TRAIN_DATA_URL = "https://storage.googleapis.com/kubric-hiring/linreg_train.csv"
-TEST_DATA_URL = "https://storage.googleapis.com/kubric-hiring/linreg_test.csv"
+def predict_price(area):
+    df = pd.read_csv('TRAIN_DATA')
+    df = df.T
+    
+    b = df.index
+    a = list(b[1:]) #area
+    
+    a = list(df[0])
+    p = a[1:] #price
+    
+    x_train = np.array(a).reshape(-1, 1)
+    y_train = np.array(p).reshape(-1, 1)
+    reg = LinearRegression.fit(x_train, y_train)
+    res = reg.predict(a)
+    return res
+    
 
 
-def predict_price(area) -> float:
-    """
-    This method must accept as input an array `area` (represents a list of areas sizes in sq feet) and must return the respective predicted prices (price per sq foot) using the linear regression model that you build.
-
-    You can run this program from the command line using `python3 regression.py`.
-    """
-    response = requests.get(TRAIN_DATA_URL)
-    # YOUR IMPLEMENTATION HERE
-    ...
-
-
-if __name__ == "__main__":
+if _name_ == "_main_":
     # DO NOT CHANGE THE FOLLOWING CODE
     from data import validation_data
-    areas = numpy.array(list(validation_data.keys()))
+    areas = numpy.array(list(validation_data.keys())).reshape(-1, 1)
     prices = numpy.array(list(validation_data.values()))
     predicted_prices = predict_price(areas)
     rmse = numpy.sqrt(numpy.mean((predicted_prices - prices) ** 2))
